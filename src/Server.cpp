5af11b054768dd1c9de73b714abe5b4c5a299159
jtcgen <jtcgen@gmail.com>
2016-03-08 14:02:15 -0500

    Restructured file system and added Server class

---
+++ src/Server.cpp
@@ -0,0 +1,9 @@
+//
+//  Server.cpp
+//  Network Emulator
+//
+//  Created by Travis Chung on 3/8/16.
+//  Copyright © 2016 jtcgen. All rights reserved.
+//
+
+#include "Server.hpp"
---
+++ src/Server.hpp
@@ -0,0 +1,14 @@
+//
+//  Server.hpp
+//  Network Emulator
+//
+//  Created by Travis Chung on 3/8/16.
+//  Copyright © 2016 jtcgen. All rights reserved.
+//
+
+#ifndef Server_hpp
+#define Server_hpp
+
+#include <stdio.h>
+
+#endif /* Server_hpp */
